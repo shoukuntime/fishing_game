8b7fe176778645c6464893f2382b4cd7adf9d8a3
your-name <gerryearth@gmail.com>
2024-10-06 09:26:10 +0800

    1.test

--- game/settings.py
+++ game/settings.py
@@ -50,8 +50,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'game.urls'
 
 TEMPLATES = [
